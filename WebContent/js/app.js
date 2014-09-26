cd03704a3c73b62a307b0ea9c6cab1549823dad4
gssran <gssran@gmail.com>
2014-09-26 10:50:15 +0530

    First pages commit

---
+++ WebContent/js/app.js
@@ -0,0 +1,64 @@
+angular.module('employeeApp', ['ngRoute'])
+	.config(
+		[ '$routeProvider',  function($routeProvider) {
+			
+			$routeProvider.when('/', {
+				templateUrl: 'partials/main.html',
+				controller: BaseController
+			});
+			
+			$routeProvider.otherwise({
+				templateUrl: '/',
+			});
+			
+		}]).run(function($window){
+			$window.localStorage.emps="";
+		});
+function BaseController($scope, $window,EmployeeModel) {
+	$scope.employees =$window.localStorage.emps.length<1?[]:$window.localStorage.emps; 
+	 
+	$scope.showForm=false;
+	$scope.createEmployee=function(){
+		$scope.employeeModel = EmployeeModel.getModel();
+		$scope.showForm=true;
+	},
+	$scope.edit=function(employee){
+		$scope.employeeModel = employee;
+		$scope.showForm=true;
+	},
+	$scope.save=function(employee){
+		var isFormValidated = $scope.employeeForm.$valid;
+		if(isFormValidated){
+			$scope.employees.push(employee);
+			$window.localStorage.emps =$scope.employees;
+			$scope.showForm=false;
+		}
+		
+	}
+	$scope.deleteEntry = function(employee) {
+		for(var i =0 ; i< $scope.employees.length;i++){
+			if(employee.empId === $scope.employees[i].empId){
+				$scope.employees.pop();
+			}
+		}
+	}
+};
+angular.module('employeeApp').factory('EmployeeModel', function () {
+
+    function EmployeeModel() {
+        var self = this;        
+        self.empId="";
+        self.name="";
+        self.loc="";
+        self.dob="";
+        self.doj="";
+      }
+    function employeeModelService() {
+        this.getModel = function () {
+            return new EmployeeModel();
+        }
+    }
+    return new employeeModelService();
+});
+
+
